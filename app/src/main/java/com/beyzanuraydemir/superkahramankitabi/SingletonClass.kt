6f418ee7ce2a69b26602436793230d0934810993
beyzaaydemir <41332762+beyzaaydemir@users.noreply.github.com>
2022-04-13 10:33:59 +0300

    Initial commit

---
+++ app/src/main/java/com/beyzanuraydemir/superkahramankitabi/SingletonClass.kt
@@ -0,0 +1,9 @@
+package com.beyzanuraydemir.superkahramankitabi
+
+import android.graphics.Bitmap
+
+class SingletonClass {
+    companion object SecilenKahraman{
+        var gorsel : Bitmap?= null
+    }
+}
